443dd47b48c8b2e6d13bf37621e024418464863f
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-06-29 14:14:42 +0200

    Basic layout

---
+++ app/layout.jsx
@@ -0,0 +1,26 @@
+import '@styles/globals.css';
+
+
+export const metadata = {
+    title: 'Promptopia',
+    description: 'Discover and share a prompts'
+}
+
+const RootLayout = ({ children }) => {
+        return (
+    <html>
+          <body>
+              <div className='main'>
+                  <div className='gradient'/>
+                </div>
+                    
+              <main className='app'>
+                  {children}
+            </main>
+              
+          </body>
+    </html>
+  )
+}
+
+export default RootLayout
