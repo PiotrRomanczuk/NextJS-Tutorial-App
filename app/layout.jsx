f76fbd6fcb5f7c4a8422996a7732cb40dbc7cb73
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-06-29 16:53:23 +0200

    Adding the Provider

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,30 +1,29 @@
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
-import '@styles/globals.css';
 
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
+import "@styles/globals.css";
 
 export const metadata = {
-    title: 'Promptopia',
-    description: 'Discover and share a prompts'
-}
+  title: "Promptopia",
+  description: "Discover & share AI Prompts",
+};
 
 const RootLayout = ({ children }) => {
-        return (
-    <html>
-          <body suppressHydrationWarning={true} >
-              <div className='main'>
-                  <div className='gradient'/>
-                </div>
-                    
-              <main className='app'>
-                <Nav />
-                <Provider />
-                  {children}
-            </main>
-              
-          </body>
+  return (
+    <html lang="en">
+      <body>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
+      </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout
