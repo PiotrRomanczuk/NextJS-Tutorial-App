db62d4fe684734363eb7cb08906561573901d6eb
PiotrRomanczuk <p.romanczuk@gmail.com>
2023-07-12 14:25:17 +0200

    0auth with google works

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,32 +1,31 @@
-
-import Nav from "@components/Nav";
-import Provider from "@components/Provider";
-import "@styles/globals.css";
-import ErrorBoundary from "@utils/ErrorBoundary";
+import Nav from '@components/Nav';
+import Provider from '@components/Provider';
+import '@styles/globals.css';
+// import ErrorBoundary from "@utils/ErrorBoundary";
 
 export const metadata = {
-  title: "Promptopia",
-  description: "Discover & share AI Prompts",
+	title: 'Promptopia',
+	description: 'Discover & share AI Prompts',
 };
 
 const RootLayout = ({ children }) => {
-  return (
-    <html lang="en">
-      <body>
-          <ErrorBoundary>
-            <Provider>
-              <div className="main">
-                <div className="gradient" />
-              </div>
-              <main className="app">
-                <Nav />
-                {children}
-              </main>
-            </Provider>
-        </ErrorBoundary>
-      </body>
-    </html>
-  );
+	return (
+		<html lang='en'>
+			<body>
+				{/* <ErrorBoundary> */}
+				<Provider>
+					<div className='main'>
+						<div className='gradient' />
+					</div>
+					<main className='app'>
+						<Nav />
+						{children}
+					</main>
+				</Provider>
+				{/* </ErrorBoundary> */}
+			</body>
+		</html>
+	);
 };
 
-export default RootLayout
+export default RootLayout;
